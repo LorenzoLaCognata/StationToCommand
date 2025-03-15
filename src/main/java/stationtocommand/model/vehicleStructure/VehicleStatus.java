f93704db8df7866507b05cac9b23e2e9c27c09c2
vodevil89 <vodevil@live.com>
2025-03-15 07:37:30 +0100

    Added Vehicle Status

---
+++ src/main/java/stationtocommand/model/vehicleStructure/VehicleStatus.java
@@ -0,0 +1,29 @@
+package stationtocommand.model.vehicleStructure;
+
+import java.util.Objects;
+
+public enum VehicleStatus {
+    AVAILABLE("Available", "/images/status/availableStatus.png"),
+    DISPATCHED("Dispatched", "/images/status/dispatchedStatus.png"),
+    ON_SCENE("On Scene", "/images/status/onSceneStatus.png"),
+    RETURNING("Returning", "/images/status/returningStatus.png"),
+    UNAVAILABLE("Unavailable", "/images/status/unavailableStatus.png");
+
+    private final String name;
+    private final String resourcePath;
+
+    VehicleStatus(String name, String resourcePath) {
+        this.name = name;
+        this.resourcePath = resourcePath;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+    public String getResourcePath() {
+        return Objects.requireNonNullElse(resourcePath, "/images/blank.png");
+    }
+
+}
